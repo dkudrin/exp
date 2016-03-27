15f7544f439e3fd49dbc72c0f6444f123b823fab
dkudrin <dkudrin@gmail.com>
2016-03-27 16:45:13 +0000

    Try bem

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,31 +1,40 @@
-'use strict';
-
-const gulp = require('gulp');
-const webpack = require('webpack');
-const gutil = require("gulp-util");
+import gulp from 'gulp'
+import webpack from 'webpack-stream'
+import gutil from "gulp-util"
 import sass from 'gulp-sass'
 import del from 'del'
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
-gulp.task('back', function (cb) {
-  webpack(require('./webpack.config.back.babel.js'), function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        cb();
-    });
+// gulp.task('back', function (cb) {
+//   webpack(require('./webpack.config.back.babel.js'), function(err, stats) {
+//         if(err) throw new gutil.PluginError("webpack", err);
+//         gutil.log("[webpack]", stats.toString({
+//             // output options
+//         }));
+//         cb();
+//     });
+// });
+
+// gulp.task('front', function (cb) {
+//   webpack(require('./webpack.config.front.babel.js'), function(err, stats) {
+//         if(err) throw new gutil.PluginError("webpack", err);
+//         gutil.log("[webpack]", stats.toString({
+//             // output options
+//         }));
+//         cb();
+//     });
+// });
+
+gulp.task('bem', function (cb) {
+  gulp.src([
+    './srcfront/man/css/main.scss',
+    './srcfront/menu/css/main.scss'
+    ])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./binfront'));
+  cb();
 });
 
-gulp.task('front', function (cb) {
-  webpack(require('./webpack.config.front.babel.js'), function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        cb();
-    });
-});
 
 gulp.task('sass', function (cb) {
   gulp.src('./srcfront/css/main.scss')
---
+++ webpack.config.bem.babel.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    '/js/app': './srcfront/js/app',
+    '/js/boots': './srcfront/js/boots'
+  },
+  output: {
+    path: __dirname + '/binfront',
+    filename: '[name].js',
+    library: "fg"
+  },
+  devtool: "cheap-inline-module-source-map",
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "/js/common"
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    }),
+    new ExtractTextPlugin('my.css')
+  ],
+  
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /\/node_modules\//,
+      loader: 'babel',
+      query: {
+        presets: ['react','es2015'],
+        plugins: [
+          ["transform-runtime", {
+            "polyfill": true,
+            "regenerator": true
+          }]
+        ]
+      }
+    },
+    {
+      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      include: /\/node_modules\//,
+      loader: 'file?name=[1].[ext]&regExp=node_modules/(.*)'
+    },
+    {
+      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      exclude: /\/node_modules\//,
+      loader: 'file?name=[path][name].[ext]'
+    }
+    ]
+  },
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+};
