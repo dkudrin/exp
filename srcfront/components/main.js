9105147bf65d761a5694decb919484da0d21f010
dkudrin <dkudrin@gmail.com>
2016-04-02 10:42:30 +0000

    Add theme essentia

--- srcfront/components/main.js
+++ srcfront/components/main.js
@@ -2,6 +2,7 @@ require('./main.scss')
 import React from 'react'
 import ReactDOM from 'react-dom'
 import page from './page'
+import theme from './themes/essentia/essentia'
 import photo from './photo'
 import work from './work'
 
--- srcfront/components/page.js
+++ srcfront/components/page.js
@@ -1,2 +1,1 @@
 require('./page.scss')
-alert('page')
