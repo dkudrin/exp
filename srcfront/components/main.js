19a6f5d8a378ac123ab15b6a003e9388b00e091a
dkudrin <dkudrin@gmail.com>
2016-04-02 11:22:55 +0000

    Clean from themes

--- srcfront/components/main.js
+++ srcfront/components/main.js
@@ -2,7 +2,6 @@ require('./main.scss')
 import React from 'react'
 import ReactDOM from 'react-dom'
 import page from './page'
-import theme from './themes/essentia/essentia'
 import photo from './photo'
 import work from './work'
 
